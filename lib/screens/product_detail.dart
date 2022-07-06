fda4115bdf2933b69f161d53726e92c3401c9968
Umar-Waseem <umar.waseem@gmail.com>
2022-07-06 23:32:19 +0500

    product detail minimal styling done

--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -17,10 +17,35 @@ class ProductDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
-      body: ListView(
-        children: [
-          Image.network(loadedProduct.imageUrl),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 30,
+              width: double.infinity,
+              child: Text(
+                loadedProduct.price.toString(),
+                style: const TextStyle(fontSize: 20, color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 15),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
