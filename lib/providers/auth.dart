20c37f8a37d57787b2321ee6f8f7bddb0e6b2f2a
Umar-Waseem <umar.waseem@gmail.com>
2022-07-11 15:32:13 +0500

    auth.dart

---
+++ lib/providers/auth.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/widgets.dart';
+
+// class Auth with ChangeNotifier {
+//   String _token;
+//   DateTime _expiryData;
+//   String userId;
+
+//   Future<void> signUp(String email, String password) async {
+
+
+
+//   }
+
+
+// }
